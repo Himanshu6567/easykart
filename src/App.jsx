bc122945013356c60a9bf9fd3e4c4225da35d3e6
Himanshu Chandola <himanshuchandola000@gmail.com>
2022-09-14 20:36:13 +0530

    nothing

--- src/App.jsx
+++ src/App.jsx
@@ -37,7 +37,7 @@ function App() {
           <Route path="*" element={<NotFound />} />
         </Routes>
       </div>
-      <h> absdefgh</h>
+      <h>abcd</h>
       <Footer />
       <Cart> </Cart>
     </div>
